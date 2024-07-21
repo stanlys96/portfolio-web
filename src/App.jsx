f78d042448745387991381516ffac5d7499e81fc
stanlys96 <stanlyskwok@gmail.com>
2024-07-22 02:49:21 +0700

    wlao

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@
 
 import "./App.css";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Aside from "./components/Aside";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
   return (
     <>
       <Aside />
